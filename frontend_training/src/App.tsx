ce6f34c59f532a11648a5b4d733c1df873008221
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-02-13 01:35:07 +0900

    add: inputとボタンを追加

--- frontend_training/src/App.tsx
+++ frontend_training/src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>TODOアプリ</h1>
+      <div id="add-task-bar">
+        <input></input>
+        <button>追加</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
