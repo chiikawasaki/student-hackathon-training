40b5faae3327ad1d044edb60d1caf41a888aa984
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-02-13 02:37:48 +0900

    add: タスクを完了できるようにした

--- frontend_training/src/App.tsx
+++ frontend_training/src/App.tsx
@@ -1,27 +1,71 @@
 import "./App.css";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+type Task = {
+  title: string;
+  id: string;
+  isComplete: boolean;
+  isEdit: boolean;
+};
+
 function App() {
-  const [task, setTask] = useState("");
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [task, setTask] = useState({
+    title: "",
+    id: "",
+    isComplete: false,
+    isEdit: false,
+  });
+  const [tasks, setTasks] = useState<Task[]>([]);
+
   function handleAddTask(): void {
-    if (task === "") {
+    if (task.title === "") {
       return;
     }
     setTasks([...tasks, task]);
-    setTask("");
+    setTask({ title: "", id: "", isComplete: false, isEdit: false });
+  }
+
+  function handleCompleteTask(id: string): void {
+    tasks.map((task) => {
+      if (task.id === id) {
+        task.isComplete = true;
+      }
+      setTasks(tasks.filter((task) => task.isComplete === false));
+    });
   }
 
   return (
     <>
       <h1>TODOアプリ</h1>
       <div id="input-form">
-        <input value={task} onChange={(e) => setTask(e.target.value)}></input>
-        <button onClick={handleAddTask}>追加</button>
+        <input
+          value={task.title}
+          onChange={(e) =>
+            setTask({
+              title: e.target.value,
+              id: uuidv4(),
+              isComplete: false,
+              isEdit: false,
+            })
+          }
+        ></input>
+        <button id="add-task-button" onClick={handleAddTask}>
+          追加
+        </button>
       </div>
       <div id="task-list-area">
         <ul>
           {tasks.map((task, index) => (
-            <li key={index}>{task}</li>
+            <div id="task" key={index}>
+              <li>{task.title}</li>
+              <button
+                id="Complete-task-button"
+                onClick={() => handleCompleteTask(task.id)}
+              >
+                完了
+              </button>
+            </div>
           ))}
         </ul>
       </div>
