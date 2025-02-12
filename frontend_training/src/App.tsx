1ab80a9694a925ceb4a2e8c469377d9bdbe74001
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-02-13 02:01:39 +0900

    add: タスクを追加できるようにした

--- frontend_training/src/App.tsx
+++ frontend_training/src/App.tsx
@@ -1,12 +1,29 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState<string[]>([]);
+  function handleAddTask(): void {
+    if (task === "") {
+      return;
+    }
+    setTasks([...tasks, task]);
+    setTask("");
+  }
+
   return (
     <>
       <h1>TODOアプリ</h1>
-      <div id="add-task-bar">
-        <input></input>
-        <button>追加</button>
+      <div id="input-form">
+        <input value={task} onChange={(e) => setTask(e.target.value)}></input>
+        <button onClick={handleAddTask}>追加</button>
+      </div>
+      <div id="task-list-area">
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
